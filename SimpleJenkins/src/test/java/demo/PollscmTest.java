5b047e849717ad026d8c1b59b8f5fa4c01fb1290
91998 <91998@LAPTOP-R4HB7KO1>
2025-02-06 13:57:56 +0530

    pollSCM added

---
+++ SimpleJenkins/src/test/java/demo/PollscmTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollscmTest {
+	@Test
+	public void test() {
+		Reporter.log("PollSCMTest class added");
+	}
+	
+
+}
